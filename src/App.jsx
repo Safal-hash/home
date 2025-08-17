ec6e24602f7511142c5d7f243556e3e8b48fe8a3
safal-hash <safaltiwari2@gmail.com>
2025-08-16 20:55:28 -0400

    figuring it out :3

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Typewriter from "typewriter-effect/dist/core";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import starburst from "/starburst.png";
+import Footer from "./components/footer.jsx";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-primary p-2 text-secondary">
-      <div className="logo flex flex-2 flex-col items-center">
-        <img src={starburst} alt="Starburst Logo" className="w-16 h-16" />
+      <div className="logo flex flex-1 flex-col items-center">
+        <img src={starburst} alt="Starburst Logo" className="w-64 h-64" />
       </div>
-      <div className="text-center flex-3 flex flex-col">
-        <h2 className="text-2xl font-bold">SAFAL</h2>
-        <span className="font-bold text-[.25rem]">I make cool sh<span className="text-red-500">*</span>t</span>
-      </div>
+     
     </div>
+
+
   );
+  
+
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-center p-4 text-secondary">
+        Made with ❤️ by Safal
+    </footer>
+  )
+}
+
+export default Footer
